f2a46cf0f47d88c8afaae694510e2b2370d1acdb
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2023-10-29 09:32:55 +0100

    adds

--- src/components/SelectedMovie/SelectedMovie.jsx
+++ src/components/SelectedMovie/SelectedMovie.jsx
@@ -1,5 +1,5 @@
 export const SelectedMovie = props => {
-  const imgBaseUrl = 'https://image.tmdb.org/t/p/w400';
+  // const imgBaseUrl = 'https://image.tmdb.org/t/p/w400';
   console.log(props);
 
   return (
@@ -28,64 +28,3 @@ export const SelectedMovie = props => {
     </div>
   );
 };
-
-// import { Suspense } from 'react';
-// import { Outlet } from 'react-router-dom';
-// import {
-//   Wrapper,
-//   Img,
-//   WrapperDetails,
-//   Title,
-//   Description,
-//   AddInfoWrapper,
-//   AddInfoList,
-//   LinkInfo,
-// } from './SelectedMovie.styled';
-
-// export const SelectedMovie = ({
-//   items: { poster_path, title, vote_average, overview },
-//   genres,
-// }) => {
-//   const imgBaseUrl = 'https://image.tmdb.org/t/p/w400';
-
-//   return (
-//     <Wrapper>
-//       <WrapperDetails>
-//         <div>
-//           <Img src={imgBaseUrl + poster_path} alt={title} />
-//         </div>
-
-//         <div>
-//           <Title>{title}</Title>
-//           <Description>
-//             User score: {vote_average ? vote_average.toFixed(1) : 'N/A'}%
-//           </Description>
-//           <Title>Overview</Title>
-//           <Description>{overview}</Description>
-//           <Title>Genres</Title>
-//           <div>
-//             {genres.map(genre => {
-//               return <Description key={genre.id}>{genre.name}</Description>;
-//             })}
-//           </div>
-//         </div>
-//       </WrapperDetails>
-
-//       <AddInfoWrapper>
-//         <Title>Additional Information</Title>
-//         <AddInfoList>
-//           <li>
-//             <LinkInfo to="cast">Cast</LinkInfo>
-//           </li>
-//           <li>
-//             <LinkInfo to="reviews">Reviews</LinkInfo>
-//           </li>
-//         </AddInfoList>
-//       </AddInfoWrapper>
-
-//       <Suspense fallback={<div>Loadind...</div>}>
-//         <Outlet />
-//       </Suspense>
-//     </Wrapper>
-//   );
-// };
