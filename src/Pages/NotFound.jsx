6779b2b43670b9da9d2b54286fd7a0208d9796f9
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2023-10-29 19:56:53 +0100

    adds

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -2,7 +2,7 @@ const NotFound = () => {
   return (
     <main style={{ textAlign: 'center' }}>
       <b style={{ fontSize: 64 }}>404</b>
-      <p>Sorry, wfind that movie</p>
+      <p>Sorry, we couldn't find that movie</p>
     </main>
   );
 };
