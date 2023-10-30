7bcdfc48dab35dd063068013eaeb376014f67cc2
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2023-10-30 10:59:22 +0100

    1
    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,9 +4,9 @@ import { Layout } from 'components/Layout/Layout';
 
 const Home = lazy(() => import('../../pages/Home/Home'));
 const SearchMovie = lazy(() => import('../../pages/Search/Search'));
-const MovieDetails = lazy(() =>
-  import('../../pages/MovieDetails/MovieDetails')
-);
+// const MovieDetails = lazy(() =>
+//   import('../../pages/MovieDetails/MovieDetails')
+// );
 const Cast = lazy(() => import('../Cast/Cast'));
 const Reviews = lazy(() => import('../Reviews/Reviews'));
 // const NotFound = lazy(() => import('../../pages/NotFound'));
@@ -17,10 +17,10 @@ export const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="movies" element={<SearchMovie />} />
-        <Route path="movies/:movieId/" element={<MovieDetails />}>
-          <Route path="casts" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />
-        </Route>
+        {/* <Route path="movies/:movieId/" element={<MovieDetails />}> */}
+        <Route path="casts" element={<Cast />} />
+        <Route path="reviews" element={<Reviews />} />
+        {/* </Route> */}
       </Route>
       {/* <Route path="*" element={<NotFound />} /> */}
     </Routes>
