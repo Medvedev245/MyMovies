fc1415f6d45e3fc03d4664e2b162ac6b39d61a1f
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2024-03-10 13:59:51 +0000

    Deploying to gh-pages from @ Medvedev245/MyMovies@cbeab6e770579ed3855eedb930967575ed19fa8f 🚀

--- static/js/786.578e7e71.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[786],{8786:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)(n.Fragment,{children:(0,n.jsx)("div",{children:"AboutUs"})})}}}]);
-//# sourceMappingURL=786.578e7e71.chunk.js.map
